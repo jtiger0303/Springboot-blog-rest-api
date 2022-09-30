8c25599bd907b8fcb831c14c257ecaa981615ef1
Jae Hyeok Shin <jtiger0303@ajou.ac.kr>
2022-10-01 03:36:36 +0900

    Adding Sorting API & Changing hardcoded to AppConstants

--- src/main/java/com/example/springbootblogrestapi/controller/PostController.java
+++ src/main/java/com/example/springbootblogrestapi/controller/PostController.java
@@ -4,6 +4,7 @@ import com.example.springbootblogrestapi.entity.Post;
 import com.example.springbootblogrestapi.payload.PostDto;
 import com.example.springbootblogrestapi.payload.PostResponse;
 import com.example.springbootblogrestapi.service.PostService;
+import com.example.springbootblogrestapi.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,10 +29,12 @@ public class PostController {
     //get all posts rest api
     @GetMapping
     public PostResponse getAllPosts(
-            @RequestParam(value="pageNo", defaultValue = "0", required = false) int pageNo,
-            @RequestParam(value="pageSize", defaultValue = "10", required = false) int pageSize
+            @RequestParam(value="pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value="pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value= "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value="sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
     ){
-        return postService.getAllPosts(pageNo, pageSize);
+        return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
     //get post by id
--- src/main/java/com/example/springbootblogrestapi/service/impl/PostServiceImpl.java
+++ src/main/java/com/example/springbootblogrestapi/service/impl/PostServiceImpl.java
@@ -9,6 +9,7 @@ import com.example.springbootblogrestapi.service.PostService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.ResourceAccessException;
 
@@ -41,10 +42,14 @@ public class PostServiceImpl implements PostService{
     }
 
     @Override
-    public PostResponse getAllPosts(int pageNo, int pageSize) {
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir) {
+
+        //sorting
+        Sort sort= sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
 
         //create Pageable Instance
-        Pageable pageable= PageRequest.of(pageNo, pageSize);
+        Pageable pageable= PageRequest.of(pageNo, pageSize, sort);
         Page<Post> posts=postRepository.findAll(pageable);
 
         //get content for page object
