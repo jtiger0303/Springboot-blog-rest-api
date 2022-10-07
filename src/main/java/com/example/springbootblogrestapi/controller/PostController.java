112697ac266e4b72538a927137dd627d40c6494a
Jae Hyeok Shin <jtiger0303@ajou.ac.kr>
2022-10-08 04:00:11 +0900

    Adding validation

--- src/main/java/com/example/springbootblogrestapi/controller/PostController.java
+++ src/main/java/com/example/springbootblogrestapi/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -22,7 +23,7 @@ public class PostController {
 
     //Create Blog Post
     @PostMapping
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
+    public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
@@ -45,7 +46,7 @@ public class PostController {
 
     //update post by id rest api
     @PutMapping("/{id}")
-    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable(name="id") Long id){
+    public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name="id") Long id){
         PostDto postResponse=postService.updatePosts(postDto, id);
         return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
--- src/main/java/com/example/springbootblogrestapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/springbootblogrestapi/exception/GlobalExceptionHandler.java
@@ -1,16 +1,22 @@
 package com.example.springbootblogrestapi.exception;
 
 import com.example.springbootblogrestapi.payload.ErrorDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice //annotation to handle exceptions globally
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     //handle specific exceptions
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest){
@@ -33,4 +39,14 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, String> errors= new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error)->{
+          String fieldName=((FieldError)error).getField();
+          String message=error.getDefaultMessage();
+          errors.put(fieldName, message);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
