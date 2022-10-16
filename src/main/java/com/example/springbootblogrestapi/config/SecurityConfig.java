ca688508c5746c2c549f758bed0c8a670f8c8d69
Jae Hyeok Shin <jtiger0303@ajou.ac.kr>
2022-10-17 03:57:19 +0900

    Adding SignIn/SignUp API

--- src/main/java/com/example/springbootblogrestapi/config/SecurityConfig.java
+++ src/main/java/com/example/springbootblogrestapi/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.example.springbootblogrestapi.config;
 
+import com.example.springbootblogrestapi.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,6 +23,9 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -29,6 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+                .antMatchers("/api/auth/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -36,12 +44,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails bryan=User.builder().username("Bryan").password(passwordEncoder()
-                .encode("password")).roles("USER").build(); //create a new user
-        UserDetails admin=User.builder().username("admin").password(passwordEncoder()
-                .encode("admin")).roles("ADMIN").build(); //create a new user
-        return new InMemoryUserDetailsManager(bryan, admin);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    //    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails bryan=User.builder().username("Bryan").password(passwordEncoder()
+//                .encode("password")).roles("USER").build(); //create a new user
+//        UserDetails admin=User.builder().username("admin").password(passwordEncoder()
+//                .encode("admin")).roles("ADMIN").build(); //create a new user
+//        return new InMemoryUserDetailsManager(bryan, admin);
+//    }
 }
